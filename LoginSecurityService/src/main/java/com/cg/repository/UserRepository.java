9619f6f6e119dedc8f4bb5c6d91164b40cdd08af
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 13:40:41 +0530

    Update UserRepository.java

--- LoginSecurityService/src/main/java/com/cg/repository/UserRepository.java
+++ LoginSecurityService/src/main/java/com/cg/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cg.model.UserModel;
+//It indicates that the decorated class is a repository
 @Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
 UserModel findByUsername(String user);
