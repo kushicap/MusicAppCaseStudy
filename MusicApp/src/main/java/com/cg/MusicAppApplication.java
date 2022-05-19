f927e9dc24d416fdfa394a2100b1247123e09977
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 12:47:14 +0530

    Update MusicAppApplication.java

--- MusicApp/src/main/java/com/cg/MusicAppApplication.java
+++ MusicApp/src/main/java/com/cg/MusicAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//microservice that register itself to eureka server
 @EnableEurekaClient
+//starting point of Spring Application
 @SpringBootApplication
 @EnableSwagger2
 public class MusicAppApplication {
