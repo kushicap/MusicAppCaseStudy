e88d16f7d859279cb4c86a6735dfbbb85fc20f02
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 13:48:21 +0530

    Update PlaylistServiceApplication.java

--- PlaylistService/src/main/java/com/cg/PlaylistServiceApplication.java
+++ PlaylistService/src/main/java/com/cg/PlaylistServiceApplication.java
@@ -3,7 +3,9 @@ package com.cg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//it makes your Spring Boot application act as a Eureka client
 @EnableEurekaClient
+//starting point of Spring Application
 @SpringBootApplication
 public class PlaylistServiceApplication {
 
