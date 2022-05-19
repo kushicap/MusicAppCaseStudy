3344b6168fa845a13ec5362a26aec8503d762abb
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 12:38:13 +0530

    Update SongAlreadyDeletedException.java

--- MusicApp/src/main/java/com/cg/exception/SongAlreadyDeletedException.java
+++ MusicApp/src/main/java/com/cg/exception/SongAlreadyDeletedException.java
@@ -6,7 +6,7 @@ public class SongAlreadyDeletedException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+//parameterized constructor
 	public SongAlreadyDeletedException() {
 		super();
 		// TODO Auto-generated constructor stub
