0842b0b330c511a3d1722153591603e642021355
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 13:15:01 +0530

    Update DeletedAlreadyException.java

--- AlbumService/src/main/java/com/cg/exception/DeletedAlreadyException.java
+++ AlbumService/src/main/java/com/cg/exception/DeletedAlreadyException.java
@@ -6,7 +6,7 @@ public class DeletedAlreadyException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+//parameterized Constructor
 	public DeletedAlreadyException() {
 		super();
 		// TODO Auto-generated constructor stub
