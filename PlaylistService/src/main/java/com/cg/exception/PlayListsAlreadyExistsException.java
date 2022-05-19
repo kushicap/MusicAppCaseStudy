195768066350b37ee879368a271b0c2cf021c329
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 13:45:36 +0530

    Update PlayListsAlreadyExistsException.java

--- PlaylistService/src/main/java/com/cg/exception/PlayListsAlreadyExistsException.java
+++ PlaylistService/src/main/java/com/cg/exception/PlayListsAlreadyExistsException.java
@@ -6,7 +6,7 @@ public class PlayListsAlreadyExistsException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+//parameterized constuctor
 	public PlayListsAlreadyExistsException() {
 		super();
 		// TODO Auto-generated constructor stub
