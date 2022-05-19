79daf31e952d88b366154eb54921b5b6462f746e
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 13:05:07 +0530

    Update AlbumServiceApplication.java

--- AlbumService/src/main/java/com/cg/AlbumServiceApplication.java
+++ AlbumService/src/main/java/com/cg/AlbumServiceApplication.java
@@ -3,6 +3,7 @@ package com.cg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//it makes your Spring Boot application act as a Eureka client
 @EnableEurekaClient
 @SpringBootApplication
 public class AlbumServiceApplication {
