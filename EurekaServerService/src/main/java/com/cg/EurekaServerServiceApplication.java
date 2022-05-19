99ad5928d2e3f867b9427c9281a6bc27b62e8509
kushicap <102595763+kushicap@users.noreply.github.com>
2022-05-19 12:59:55 +0530

    Update EurekaServerServiceApplication.java

--- EurekaServerService/src/main/java/com/cg/EurekaServerServiceApplication.java
+++ EurekaServerService/src/main/java/com/cg/EurekaServerServiceApplication.java
@@ -3,6 +3,7 @@ package com.cg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//It make your Spring Boot application acts as a Eureka Server.
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerServiceApplication {
